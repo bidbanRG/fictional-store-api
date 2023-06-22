0e1ca74202a2848a186bafaf9945c41afb7d0e93
Bidesh <bideshbanerjee444@gmail.com>
2023-06-22 20:36:59 +0530

    packages updated

---
+++ routes/route.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+
+import { signUp,login } from "../controllers/Authentication.js";
+import { authenticateToken } from "../controllers/authController.js";
+import { getOrders,addToCard,updateOrder,deleteOrder, checkOutOneByOne} from "../controllers/orderController.js";
+import { getProduct,createProduct, updateProduct, deleteProduct } from "../controllers/productController.js";
+
+const router = express.Router();
+
+
+router.post("/login",login)
+router.post("/signUp",signUp)
+
+
+router.get("/product",authenticateToken,getProduct)
+router.post("/product",authenticateToken,createProduct)
+router.put("/product/:name",authenticateToken,updateProduct)
+router.delete("/product/:name",authenticateToken,deleteProduct)
+
+router.get("/order",authenticateToken,getOrders)
+router.post("/order",authenticateToken,addToCard)
+router.put("/order/:productName",authenticateToken,updateOrder)
+router.delete("/order/:productName",authenticateToken,deleteOrder)
+
+
+router.post("/checkout/:productName",authenticateToken,checkOutOneByOne)
+
+export default router;
---
+++ utils/utils.js
@@ -0,0 +1,5 @@
+
+export default function (param,min,max){
+    return !param || param.length < min || param.length > max;
+}
+
